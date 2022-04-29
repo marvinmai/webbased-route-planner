9ef5a5cd981f413f648233c04f62d1709665e209
marvin <marvinmai@posteo.de>
2022-04-29 17:55:35 +0200

    Fix file scanning int overflow error

--- src/main/java/de/unistuttgart/Main.java
+++ src/main/java/de/unistuttgart/Main.java
@@ -1,44 +1,48 @@
 package de.unistuttgart;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("/home/marvin/Documents/Studium/SoSe22/Programmierprojekt/datasets/toy.fmi");
+        BufferedReader reader;
         try {
-            Scanner scanner = new Scanner(file);
-            for (int i = 0; i < 4; i++) {
-                scanner.nextLine();
-            }
-            int numberOfNodes = scanner.nextInt();
-            scanner.nextLine();
-            int numberOfEdges = scanner.nextInt();
-
-            scanner.nextLine();
-            double[][] nodes = new double[numberOfNodes][2];
-            for (int i = 0; i < numberOfNodes && scanner.hasNext(); i++) {
-                scanner.nextInt();
-                scanner.nextInt();
-                nodes[i][0] = scanner.nextDouble();
-                nodes[i][1] = scanner.nextDouble();
-                scanner.nextLine();
-            }
-            int[] sourceNodeIds = new int[numberOfEdges];
-            int[] targetNodeIds = new int[numberOfEdges];
-            int[] sourceToTargetCosts = new int[numberOfEdges];
-            for (int i = 0; i < numberOfEdges && scanner.hasNext(); i++) {
-                sourceNodeIds[i] = scanner.nextInt();
-                targetNodeIds[i] = scanner.nextInt();
-                sourceToTargetCosts[i] = scanner.nextInt();
-                scanner.nextLine();
-            }
-            System.out.println("");
-
+            FileReader fileReader = new FileReader("/home/marvin/Documents/Studium/SoSe22/Programmierprojekt/datasets/MV.fmi");
+            reader = new BufferedReader(fileReader);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
 
+        try {
+            for (int i = 0; i < 5; i++) {
+                reader.readLine();
+            }
+
+            int numberOfNodes = Integer.parseInt(reader.readLine());
+            int numberOfEdges = Integer.parseInt(reader.readLine());
+            String[] lineElements;
+            double[][] nodes = new double[numberOfNodes][2];
+            for (int i = 0; i < numberOfNodes; i++) {
+                lineElements = reader.readLine().split(" ");
+                nodes[i][0] = Double.parseDouble(lineElements[2]);
+                nodes[i][1] = Double.parseDouble(lineElements[3]);
+            }
+
+            int[] sourceNodeIds = new int[numberOfEdges];
+            int[] targetNodeIds = new int[numberOfEdges];
+            int[] sourceToTargetCosts = new int[numberOfEdges];
+            for (int i = 0; i < numberOfEdges; i++) {
+                lineElements = reader.readLine().split(" ");
+
+                sourceNodeIds[i] = Integer.parseInt(lineElements[0]);
+                targetNodeIds[i] = Integer.parseInt(lineElements[1]);
+                sourceToTargetCosts[i] = Integer.parseInt(lineElements[2]);
+            }
+            System.out.println();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
