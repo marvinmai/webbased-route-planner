d91cf41352e293408682efb269c728b71487bc70
marvin <marvinmai@posteo.de>
2022-04-29 20:14:07 +0200

    Add data structure DataSet

---
+++ src/main/java/de/unistuttgart/DataSet.java
@@ -0,0 +1,36 @@
+package de.unistuttgart;
+
+public class DataSet {
+    private int numberOfNodes;
+    private int numberOfEdges;
+
+    private double[][] nodes;
+
+    private int[] sourceNodeIds;
+    private int[] targetNodeIds;
+    private int[] sourceToTargetCosts;
+
+    public DataSet(int numberOfNodes, int numberOfEdges) {
+        this.numberOfNodes = numberOfNodes;
+        this.numberOfEdges = numberOfEdges;
+
+        nodes = new double[numberOfNodes][2];
+
+        sourceNodeIds = new int[numberOfEdges];
+        targetNodeIds = new int[numberOfEdges];
+        sourceToTargetCosts = new int[numberOfEdges];
+    }
+
+    public void addNode(int index, double firstValue, double secondValue) {
+        nodes[index][0] = firstValue;
+        nodes[index][1] = secondValue;
+    }
+
+    public void addEntry(int index, int sourceNodeId, int targetNodeId, int sourceToTargetCost) {
+        sourceNodeIds[index] = sourceNodeId;
+        targetNodeIds[index] = targetNodeId;
+        sourceToTargetCosts[index] = sourceToTargetCost;
+    }
+}
+
+
--- src/main/java/de/unistuttgart/Main.java
+++ src/main/java/de/unistuttgart/Main.java
@@ -7,12 +7,15 @@ import java.io.IOException;
 
 public class Main {
 
-    private static long currentIndex = 0;
+    private static DataSet dataSet;
     public static void main(String[] args) {
         long start = System.nanoTime() / 1000000;
         BufferedReader reader;
+
+        String filePath = "/home/marvin/Documents/Studium/SoSe22/Programmierprojekt/datasets/toy.fmi";
+
         try {
-            FileReader fileReader = new FileReader("/home/marvin/Documents/Studium/SoSe22/Programmierprojekt/datasets/germany.fmi");
+            FileReader fileReader = new FileReader(filePath);
             reader = new BufferedReader(fileReader);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -25,31 +28,27 @@ public class Main {
 
             int numberOfNodes = Integer.parseInt(reader.readLine());
             int numberOfEdges = Integer.parseInt(reader.readLine());
+
+            dataSet = new DataSet(numberOfNodes, numberOfEdges);
+
             String[] lineElements;
-            double[][] nodes = new double[numberOfNodes][2];
             for (int i = 0; i < numberOfNodes; i++) {
-                currentIndex = i;
                 lineElements = reader.readLine().split(" ");
-                nodes[i][0] = Double.parseDouble(lineElements[2]);
-                nodes[i][1] = Double.parseDouble(lineElements[3]);
+                dataSet.addNode(i, Double.parseDouble(lineElements[2]), Double.parseDouble(lineElements[3]));;
             }
 
-            int[] sourceNodeIds = new int[numberOfEdges];
-            int[] targetNodeIds = new int[numberOfEdges];
-            int[] sourceToTargetCosts = new int[numberOfEdges];
             for (int i = 0; i < numberOfEdges; i++) {
-                currentIndex = i;
                 lineElements = reader.readLine().split(" ");
 
-                sourceNodeIds[i] = Integer.parseInt(lineElements[0]);
-                targetNodeIds[i] = Integer.parseInt(lineElements[1]);
-                sourceToTargetCosts[i] = Integer.parseInt(lineElements[2]);
+                dataSet.addEntry( i,
+                        Integer.parseInt(lineElements[0]),
+                        Integer.parseInt(lineElements[1]),
+                        Integer.parseInt(lineElements[2]));
             }
             System.out.println();
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (NullPointerException e) {
-            System.out.println(currentIndex);
             e.printStackTrace();
         }
         long duration = System.nanoTime() / 1000000 - start;
