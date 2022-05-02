e3e7f5903d55ca631fd81424978a61f7e799aa8f
covid19-testresults <98493418+covid19-testresults@users.noreply.github.com>
2022-05-02 15:40:00 +0200

    Added getter methodes for the fields of DataSet class

---
+++ src/main/java/de/unistuttgart/Dijkstra.java
@@ -0,0 +1,14 @@
+package de.unistuttgart;
+
+
+public class Dijkstra {
+    private DataSet dataSet = new DataSet();
+    public void findTheShortestPathToTheNextNode(int srcNodeID){
+        double[][] nodes = dataSet.getNodes();
+        double[][
+        nodes
+
+    }
+
+
+}
