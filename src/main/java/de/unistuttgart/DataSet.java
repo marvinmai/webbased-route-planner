6ee131a2f2b17d94fbf2b942a316d6e4b6bc56ac
marvin <marvinmai@posteo.de>
2022-05-04 20:07:37 +0200

    Move method for square filtering to NearestNodeSelector

--- src/main/java/de/unistuttgart/DataSet.java
+++ src/main/java/de/unistuttgart/DataSet.java
@@ -42,42 +42,6 @@ public class DataSet {
     public double[][] getNodes() {
         return nodes;
     }
-
-    /**
-     * Filtering nodes by putting a square around a given coordinate. The coordinate is located
-     * in the middle of the square.
-     *
-     * @param latNode latitude of the coordinate
-     * @param longNode longitude of the coordinate
-     * @param squareSizeDegrees Side-length of the square in degrees. 0.1deg ~= 11.0574km
-     * @return all nodes inside the given square
-     */
-    public List<Node> getNodesInSquareAround(double latNode, double longNode, double squareSizeDegrees) {
-        List<Node> filteredNodes = new ArrayList<>();
-
-        double lowerLong = longNode - squareSizeDegrees / 2;
-        double upperLong = longNode + squareSizeDegrees / 2;
-        double lowerLat =  latNode -  squareSizeDegrees / 2;
-        double upperLat = latNode + squareSizeDegrees / 2;
-
-        double[] currentNode;
-        for (int i = 0; i < nodes.length; i++) {
-            currentNode = nodes[i];
-            if (longitude(currentNode) > lowerLong && longitude(currentNode) < upperLong
-                    && latitude(currentNode) > lowerLat && latitude(currentNode) < upperLat) {
-                filteredNodes.add(new Node(i, latitude(currentNode), longitude(currentNode)));
-            }
-        }
-        return filteredNodes;
-    }
-
-    private double latitude(double[] node) {
-        return node[0];
-    }
-
-    private double longitude(double[] node) {
-        return node[1];
-    }
 }
 
 
--- src/main/java/de/unistuttgart/NearestNodeSelector.java
+++ src/main/java/de/unistuttgart/NearestNodeSelector.java
@@ -1,5 +1,6 @@
 package de.unistuttgart;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NearestNodeSelector {
@@ -15,7 +16,7 @@ public class NearestNodeSelector {
         double distance;
         Node nearestNode = null;
 
-        List<Node> filteredNodes = dataSet.getNodesInSquareAround(clickLatitude, clickLongitude, 0.1);
+        List<Node> filteredNodes = getNodesInSquareAround(clickLatitude, clickLongitude, 0.1);
 
         for (Node node: filteredNodes) {
 
@@ -33,4 +34,39 @@ public class NearestNodeSelector {
         return Math.pow(Math.abs(lat2 - lat1), 2) + Math.pow(Math.abs(long2 - long1), 2);
     }
 
+    /**
+     * Filtering nodes by putting a square around a given coordinate. The coordinate is located
+     * in the middle of the square.
+     *
+     * @param latNode latitude of the coordinate
+     * @param longNode longitude of the coordinate
+     * @param squareSizeDegrees Side-length of the square in degrees. 0.1deg ~= 11.0574km
+     * @return all nodes inside the given square
+     */
+    public List<Node> getNodesInSquareAround(double latNode, double longNode, double squareSizeDegrees) {
+        List<Node> filteredNodes = new ArrayList<>();
+
+        double lowerLong = longNode - squareSizeDegrees / 2;
+        double upperLong = longNode + squareSizeDegrees / 2;
+        double lowerLat =  latNode -  squareSizeDegrees / 2;
+        double upperLat = latNode + squareSizeDegrees / 2;
+
+        double[] currentNode;
+        for (int i = 0; i < dataSet.getNodes().length; i++) {
+            currentNode = dataSet.getNodes()[i];
+            if (longitude(currentNode) > lowerLong && longitude(currentNode) < upperLong
+                    && latitude(currentNode) > lowerLat && latitude(currentNode) < upperLat) {
+                filteredNodes.add(new Node(i, latitude(currentNode), longitude(currentNode)));
+            }
+        }
+        return filteredNodes;
+    }
+
+    private double latitude(double[] node) {
+        return node[0];
+    }
+
+    private double longitude(double[] node) {
+        return node[1];
+    }
 }
