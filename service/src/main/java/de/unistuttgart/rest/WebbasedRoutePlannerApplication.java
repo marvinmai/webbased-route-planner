9f961af077b9e55b5f5bbe385856da015102ab48
marvin <marvinmai@posteo.de>
2022-05-25 22:55:46 +0200

    Cleaning code

--- service/src/main/java/de/unistuttgart/rest/WebbasedRoutePlannerApplication.java
+++ service/src/main/java/de/unistuttgart/rest/WebbasedRoutePlannerApplication.java
@@ -1,11 +1,7 @@
 package de.unistuttgart.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WebbasedRoutePlannerApplication {
--- service/src/main/java/de/unistuttgart/rest/errorhandling/DataReadAlreadyInProgressException.java
+++ service/src/main/java/de/unistuttgart/rest/errorhandling/DataReadAlreadyInProgressException.java
@@ -1,8 +1,5 @@
 package de.unistuttgart.rest.errorhandling;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class DataReadAlreadyInProgressException extends Throwable {
     public DataReadAlreadyInProgressException() {
         super();
