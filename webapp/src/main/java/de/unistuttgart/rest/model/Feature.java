61d340abebea3273b711b9a3f28ddea1ce1832d7
marvin <marvinmai@posteo.de>
2022-06-14 17:24:20 +0200

    Add interface to rest service that returns a path as GeoJson

---
+++ webapp/src/main/java/de/unistuttgart/rest/model/Feature.java
@@ -0,0 +1,74 @@
+
+package de.unistuttgart.rest.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import de.unistuttgart.rest.model.Geometry;
+import de.unistuttgart.rest.model.Properties;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "type",
+    "geometry",
+    "properties"
+})
+@Generated("jsonschema2pojo")
+public class Feature {
+
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("geometry")
+    private Geometry geometry;
+    @JsonProperty("properties")
+    private Properties properties;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("geometry")
+    public Geometry getGeometry() {
+        return geometry;
+    }
+
+    @JsonProperty("geometry")
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
+    }
+
+    @JsonProperty("properties")
+    public Properties getProperties() {
+        return properties;
+    }
+
+    @JsonProperty("properties")
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ webapp/src/main/java/de/unistuttgart/rest/model/Properties.java
@@ -0,0 +1,46 @@
+
+package de.unistuttgart.rest.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "marker-symbol"
+})
+@Generated("jsonschema2pojo")
+public class Properties {
+
+    @JsonProperty("marker-symbol")
+    private String markerSymbol;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("marker-symbol")
+    public String getMarkerSymbol() {
+        return markerSymbol;
+    }
+
+    @JsonProperty("marker-symbol")
+    public void setMarkerSymbol(String markerSymbol) {
+        this.markerSymbol = markerSymbol;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
