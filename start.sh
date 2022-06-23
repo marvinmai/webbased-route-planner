a0e59a029aca57991039689dabfb3772c4218206
marvin <marvinmai@posteo.de>
2022-06-23 15:31:24 +0200

    Add max memory parameter to java command in start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 ## replace path with your correct path to the file "germany.fmi"
-java -jar webapp/target/webapp-1.0.0.jar /home/marvin/dev/studium-dev/Programmierprojekt/datasets/germany.fmi
+java -jar -Xmx8G webapp/target/webapp-1.0.0.jar /home/marvin/dev/studium-dev/Programmierprojekt/datasets/germany.fmi
